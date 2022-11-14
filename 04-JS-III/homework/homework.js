555e487b3f7e2f75d7a78d3c29cfced7e5fea945
JoseLuisHcoder <zonajl@gmail.com>
2022-11-14 08:58:36 -0400

    update

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -134,7 +134,12 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+let cont = 0
+for(let i=0; i<arreglo.length; i++){
+  if(arreglo[i] > 18)
+  cont++
+}
+return cont
 }
 
 
@@ -143,7 +148,8 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
- 
+ if(numeroDeDia === 1 || numeroDeDia === 7) return "Es fin de semana"
+ return "Es dia Laboral"
  
 } 
 
@@ -152,7 +158,9 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  let str = n.toString()
+  if(Number(str[0]) === 9) return true
+  return false
 
 }
 
@@ -161,8 +169,14 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for(let i=0; i<arreglo.length; i++){
+    if(arreglo[i] === arreglo[i + 1]){
+      return true
     }
+  }
+  return false
+
+  }
     
 
 
@@ -173,6 +187,18 @@ function mesesDelAño(array) {
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
   
+  let meses = []
+  for(let i=0; i<array.length; i++){
+    if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre"){
+      meses.push(array[i])
+    }
+  
+
+  }
+  if(meses.length < 3){
+    return "No se encontraron los meses pedidos"
+  } 
+  return meses
 }
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
